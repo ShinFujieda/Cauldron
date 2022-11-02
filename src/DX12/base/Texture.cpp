8269e2e8f6bee93dfc5c98e7e318ea2884f2842e
ShinFujieda <Shin.Fujieda@amd.com>
2022-11-02 18:17:47 +0900

    [FPS-0] Add the way of setting 64-bit stride in CreateBufferUAV

--- src/DX12/base/Texture.cpp
+++ src/DX12/base/Texture.cpp
@@ -298,7 +298,7 @@ namespace CAULDRON_DX12
         CreateUAV(index, NULL, pRV, &uavDesc);
     }
 
-    void Texture::CreateBufferUAV(uint32_t index, Texture* pCounterTex, CBV_SRV_UAV* pRV)
+    void Texture::CreateBufferUAV(uint32_t index, Texture* pCounterTex, CBV_SRV_UAV* pRV, bool hack64bit)
     {
         D3D12_RESOURCE_DESC resourceDesc = m_pResource->GetDesc();
 
@@ -306,8 +306,8 @@ namespace CAULDRON_DX12
         uavDesc.Format = m_header.format;
         uavDesc.ViewDimension = D3D12_UAV_DIMENSION_BUFFER;
         uavDesc.Buffer.FirstElement = 0;
-        uavDesc.Buffer.NumElements = m_header.width;
-        uavDesc.Buffer.StructureByteStride = m_structuredBufferStride;
+        uavDesc.Buffer.NumElements = hack64bit ? m_header.width/2 : m_header.width;
+        uavDesc.Buffer.StructureByteStride = hack64bit ? 2*m_structuredBufferStride : m_structuredBufferStride;
         uavDesc.Buffer.CounterOffsetInBytes = 0;
         uavDesc.Buffer.Flags = D3D12_BUFFER_UAV_FLAG_NONE;
 
--- src/DX12/base/Texture.h
+++ src/DX12/base/Texture.h
@@ -50,7 +50,7 @@ namespace CAULDRON_DX12
         // less explicit functions of the above ones
         void CreateDSV(uint32_t index, DSV *pRV, int arraySlice = -1, int arraySize = 1);
         void CreateUAV(uint32_t index, CBV_SRV_UAV *pRV, int mipLevel = -1);
-        void CreateBufferUAV(uint32_t index, Texture *pCounterTex, CBV_SRV_UAV *pRV);
+        void CreateBufferUAV(uint32_t index, Texture *pCounterTex, CBV_SRV_UAV *pRV, bool hack64bit = false);
         void CreateRTV(uint32_t index, RTV *pRV, int mipLevel = -1, int arraySize = -1, int firstArraySlice = -1, DXGI_FORMAT format = DXGI_FORMAT_UNKNOWN);
         void CreateSRV(uint32_t index, CBV_SRV_UAV *pRV, int mipLevel = -1, int arraySize = -1, int firstArraySlice = -1);
         void CreateCubeSRV(uint32_t index, CBV_SRV_UAV *pRV);
